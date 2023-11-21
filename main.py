648c718c3236b5e1f556d9be6b7d67bf2c308909
gvnff <ffgeovane08@gmail.com>
2023-11-21 16:35:22 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#programa de média de notas
+#variaveis nota1 e nota2 recebem valores do tipo float do usuario
+nota1= float(input('Digite a primeira Nota '))
+nota2= float(input('Digite a Segunda Nota '))
+#resultado recebe um valor
+resultado= (nota1 + nota2) / 2
+
+print('Sua média será ', resultado)
